6537a79a5be260ccb484c55c6d661a697dae2cf5
Paulina Wątroba <paulina@watroba.com.pl>
2020-05-17 23:14:58 +0200

    mała automatyzacja

---
+++ RunScripts.R
@@ -0,0 +1,3 @@
+source( "C:/Users/Paulina/Documents/SIR_experiments/Repository/multinet_SIR_only.R")
+source( "C:/Users/Paulina/Documents/SIR_experiments/Repository/multinet_SIS_and_SIS_default.R")
+source( "C:/Users/Paulina/Documents/SIR_experiments/Repository/multinet_SIS_and_SIS_blocking.R")
