35f75e5efcecaa905ee3bf7fb6d0570b4813f303
Paulina Wątroba <paulina@watroba.com.pl>
2020-05-27 04:14:48 +0200

    Refactor for old scripts

---
+++ choosen actors/RunScripts-faster.R
@@ -0,0 +1,3 @@
+source( "C:/Users/Paulina/Documents/MasterThesis/Repository/faster_multinet_SIR_only.R")
+source( "C:/Users/Paulina/Documents/MasterThesis/Repository/faster_multinet_SIS_and_SIS_default.R")
+source( "C:/Users/Paulina/Documents/MasterThesis/Repository/faster_multinet_SIS_and_SIS_blocking.R")
